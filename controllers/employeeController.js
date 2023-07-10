7bd7c643afa0ecc60278f26e988d9fa0e0db012a
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-10 18:21:12 -0400

    Got the adding functions added

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,3 +1,4 @@
+const { default: inquirer } = require('inquirer');
 const connection = require('../connection');
 
 // This shows up in every function. Now I can easily call it.
@@ -45,10 +46,73 @@ function viewAllEmployees() {
 
 // Adding functions
 // Handle adding a department
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter department name:"
+            },
+        ])
+        .then((answer) => {
+            const query = "INSERT INTO departments (name) VALUES (?)";
+            const values = [answer.name];
+
+            connection.query(query, values, (err, res) => {
+                handleError(err);
+
+                console.log("Department added successfully!");
+                mainMenu();
+            });
+        });
+}
 
 // Handle adding a role
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter name of role:"
+            },
+        ])
+        .then((answer) => {
+            const query = "INSERT INTO roles (name) VALUES (?)";
+            const values = [answer.name];
+
+            connection.query(query, values, (err, res) => {
+                handleError(err);
+
+                console.log("Role added successfully!");
+                mainMenu();
+            });
+        });
+}
 
 // Handle adding employee
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter employee name:"
+            },
+        ])
+        .then((answer) => {
+            const query = "INSERT INTO employees (name) VALUES (?)";
+            const values = [answer.name];
+
+            connection.query(query, values, (err, res) => {
+                handleError(err);
+
+                console.log("Employee added successfully!");
+                mainMenu();
+            });
+        });
+}
 
 
 // Update functions

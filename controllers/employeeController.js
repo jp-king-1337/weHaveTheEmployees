e58f482cfa49460134d67fec1473eb0be91e41dd
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-10 18:12:33 -0400

    Added the remaining viewing functions

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -20,8 +20,28 @@ function viewAllDepartments() {
 }
 
 // Handle viewing all roles
+function viewAllRoles() {
+    const query = "SELECT * FROM roles";
+
+    connection.query(query, (err, res) => {
+        handleError(err);
+
+        console.table(res);
+        mainMenu();
+    });
+}
 
 // Function to handle viewing all employees
+function viewAllEmployees() {
+    const query = "SELECT * FROM employees";
+
+    connection.query(query, (err, res) => {
+        handleError(err);
+
+        console.table(res);
+        mainMenu();
+    });
+}
 
 // Adding functions
 // Handle adding a department

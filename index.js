db85713db3e98014ce312b212db96ed34451d218
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-10 17:04:54 -0400

    Made the basics of the mainMenu. Need lots of functions now

--- index.js
+++ index.js
@@ -2,3 +2,58 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 // I know I need main menu
+function mainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "What would you like to do?",
+                choices: [
+                    "View All Employees",
+                    "Add Employee",
+                    "Update Employee Role",
+                    "View All Roles",
+                    "Add Role",
+                    "View All Departments",
+                    "Add Department"
+                    // BONUS:
+                    // "Update Employee Managers",
+                    // "View By Manager",
+                    // "View By Department",
+                    // "Delete Options", // Departments, roles, employees
+                    // "View Budget" // View total utilized budget of a department - the combined salaries of all employees in that department
+                ]
+            }
+        ])
+        .then((answer) => {
+            switch (answer.action) {
+                case "View All Employees":
+                    // Function to handle viewing all employees
+                    break;
+                case "Add Employee":
+                    // Handle adding employee
+                    break;
+                case "Update Employee Role":
+                    // Handle updating employee role
+                    break;
+                case "View All Roles":
+                    // Handle viewing all roles
+                    break;
+                case "Add Role":
+                    // Handle adding a role
+                    break;
+                case "View All Departments":
+                    // Handle viewing all departments
+                    break;
+                case "Add Department":
+                    // Handle adding a department
+                    break;
+                // BONUSes here
+                default:
+                    break;
+            }
+        });
+}
+
+mainMenu();

8fdede471b7e78c570f209d6263b876402422516
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-10 22:35:20 -0400

    Working on database issues

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,15 +14,15 @@ VALUES
     ("Account Manager", 3, 160000),
     ("Accountant", 3, 125000),
     ("Legal Team Lead", 4, 250000),
-    ("Lawyer", 4, 190000)
+    ("Lawyer", 4, 190000);
 
-INSERT INTO employees (first_name, last_name, role_id, department_id, salary, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ("John", "Doe", 1, 1, 100000, NULL),
-  ("Mike", "Chan", 2, 1, 80000, "John Doe"),
-  ("Ashley", "Rodriguez", 3, 2, 150000, NULL),
-  ("Kevin", "Tupik", 4, 2, 120000, "Ashley Rodriguez"),
-  ("Kunal", "Singh", 5, 3, 160000, NULL),
-  ("Malia", "Brown", 6, 3, 125000, "Kunal Singh"),
-  ("Sarah", "Lourd", 7, 4, 250000, NULL),
-  ("Tom", "Allen", 8, 4, 190000, "Sarah Lourd");
+    ("John", "Doe", 1, NULL),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 3, NULL),
+    ("Kevin", "Tupik", 4, 3),
+    ("Kunal", "Singh", 5, NULL),
+    ("Malia", "Brown", 6, 5),
+    ("Sarah", "Lourd", 7, NULL),
+    ("Tom", "Allen", 8, 7);
